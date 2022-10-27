b1ee3dcc7ae90e3e8a888ab153d45868ed46344f
Adam Bonsu <adam.bonsu@itv.com>
2022-10-27 18:54:26 +0100

    remove siteprism refs from gemfiles

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -3,5 +3,5 @@
 Before do
   Capybara.current_session.visit('http://www.google.com') # browser launches and loads google.com
   sleep 5
-  Capybara.current_session.visit('https://example.com/') # Net::ReadTimeout: Net::ReadTimeout with #<TCPSocket:(closed)>
+  Capybara.current_session.visit('https://example.com/') # Net::ReadTimeout with #<TCPSocket:(closed)> (Net::ReadTimeout)
 end
